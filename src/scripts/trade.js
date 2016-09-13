0c363fc1524536de1693de2820cb2b130ab0d228
huwanqi <huwanqi1234@gmail.com>
2016-09-13 14:17:27 +0800

    huwanqi: 发射效果fix

--- src/scripts/trade.js
+++ src/scripts/trade.js
@@ -16,36 +16,78 @@ class Trade {
     render(){
         const { trade, src, dest } = this;
 
-        // path
-        //svg.append('path')
-        //    .transition()
-        //    .duration(3000)
-        //    .attr('opacity', 0.2)
-        //    .attr('stroke', 'red')
-        //    .attr('stroke-width', '2px')
-        //    .attr('d', 'M' + src[0] + ' ' + src[1] + 'L' + dest[0] + ' ' + dest[1]);
-
         // line
         const length = distanceBetweenTwoPoints(dest, src);
         const middle = [(dest[0] + src[0]) / 2, (dest[1] + src[1]) / 2];
+        svg.append('circle')
+            .attr('cx', src[0])
+            .attr('cy', src[1])
+            .attr('r', 1)
+            .attr('opacity', 0.8)
+            .attr('fill', 'none')
+            .attr('stroke', 'red')
+            .attr('stroke-width', '2px')
+            .transition()
+            .duration(1000)
+            .attr('r', 40)
+            .attr('opacity', 0);
+
         const meteor = svg.append('line')
             .attr('stroke', 'red')
             .attr('stroke-width', '4px')
+            .attr('opacity', 0.8)
+            .attr('class', 'trade-line')
             .attr('x1', src[0])
             .attr('y1', src[1])
             .attr('x2', src[0] + 20 / length * (dest[0] - src[0]))
             .attr('y2', src[1] + 20 / length * (dest[1] - src[1]));
 
         meteor.transition()
-            .duration(2000)
-            .ease((t) => {
-                return 1 - Math.sqrt(1 - t * t); // circle
-                return t < 1 / 2.75 ? 7.5625 * t * t : t < 2 / 2.75 ? 7.5625 * (t -= 1.5 / 2.75) * t + .75 : t < 2.5 / 2.75 ? 7.5625 * (t -= 2.25 / 2.75) * t + .9375 : 7.5625 * (t -= 2.625 / 2.75) * t + .984375;
-            })
-            .attr('x1', dest[0] - 40 / length * (dest[0] - src[0]))
-            .attr('y1', dest[1] - 40 / length * (dest[1] - src[1]))
+            .duration(2000).attr('x1', dest[0] - 0 / length * (dest[0] - src[0]))
+            .attr('y1', dest[1] - 0 / length * (dest[1] - src[1]))
             .attr('x2', dest[0])
-            .attr('y2', dest[1]);
+            .attr('y2', dest[1])
+            .each('end', () => {
+                svg.append('circle')
+                    .attr('cx', dest[0])
+                    .attr('cy', dest[1])
+                    .attr('r', 1)
+                    .attr('opacity', 0.8)
+                    .attr('fill', 'none')
+                    .attr('stroke', 'red')
+                    .attr('stroke-width', '2px')
+                    .transition()
+                    .duration(1000)
+                    .attr('r', 40)
+                    .attr('opacity', 0);
+            });
+
+        const makeGhost = (index) => {
+            const ghost = svg.append('line')
+                .attr('stroke', 'red')
+                .attr('stroke-width', '4px')
+                .attr('opacity', 0.01 * index)
+                .attr('class', 'trade-line')
+                .attr('x1', src[0])
+                .attr('y1', src[1])
+                .attr('x2', src[0] + 20 / length * (dest[0] - src[0]))
+                .attr('y2', src[1] + 20 / length * (dest[1] - src[1]));
+
+            ghost.transition()
+                .duration(2000).attr('x1', dest[0] - 0 / length * (dest[0] - src[0]))
+                .attr('y1', dest[1] - 0 / length * (dest[1] - src[1]))
+                .attr('x2', dest[0])
+                .attr('y2', dest[1])
+                .each('end', () => {
+                    ghost.remove();
+                });
+        };
+
+        for(let i = 39; i >= 1; i--) {
+            setTimeout(() => {
+                makeGhost(i);
+            }, 5 * (40 - i));
+        }
 
         // circle with transition
         //const primitive = svg.append('circle')
